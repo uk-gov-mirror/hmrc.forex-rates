e6a89894d5fefbbd3988e1fefb74306e7aa8846f
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2026-05-12 16:16:39 +0100

    Diufferent version with pekko override versions (#35)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,17 @@ object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
+  private val pekkoVersion = "1.6.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "io.github.samueleresca"  %% "pekko-quartz-scheduler"     % "1.2.2-pekko-1.0.x"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"   % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"          % hmrcMongoVersion,
+    "io.github.samueleresca"  %% "pekko-quartz-scheduler"      % "1.3.0-pekko-1.1.x",
+    "org.apache.pekko"        %% "pekko-protobuf-v3"           % pekkoVersion,
+    "org.apache.pekko"        %% "pekko-serialization-jackson" % pekkoVersion,
+    "org.apache.pekko"        %% "pekko-stream"                % pekkoVersion,
+    "org.apache.pekko"        %% "pekko-actor-typed"           % pekkoVersion,
+    "org.apache.pekko"        %% "pekko-slf4j"                 % pekkoVersion
   )
 
   val test = Seq(
